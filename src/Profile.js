1110609f4eb07adc1ba280e4df0d324a80fcfe7f
_woo_s.j <sjwoo1999@korea.ac.kr>
2023-07-11 22:48:58 +0900

    230711 last commit

--- src/Profile.js
+++ src/Profile.js
@@ -1,32 +1,49 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
+import styled from "styled-components/native";
+import Margin from "./Margin";
+
+const Container = styled.View`
+  flex-direction: row;
+`;
+
+const ProfileImage = styled.Image`
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
+  border-radius: ${(props) => props.size * 0.4}px;
+`;
+
+const TextContainer = styled.View`
+  justify-content: center;
+  margin-left: 10px;
+`;
+
+const NameText = styled.Text`
+  font-weight: ${(props) => (props.isMe ? "bold" : "normal")};
+  font-size: ${(props) => (props.isMe ? 16 : 15)};
+`;
+
+const IntroductionText = styled.Text`
+  font-size: ${(props) => (props.isMe ? 12 : 11)};
+  color: grey;
+`;
 
 export default ({ uri, name, introduction, isMe }) => {
   const size = isMe ? 50 : 40;
   return (
-    <View style={{ flexDirection: "row" }}>
-      <Image
-        source={{ uri }}
-        style={{ width: size, height: size, borderRadius: size * 0.4 }}
-      />
-      <View style={{ justifyContent: "center", marginLeft: 10 }}>
-        <Text
-          style={{
-            fontWeight: isMe ? "bold" : undefined,
-            fontSize: isMe ? 16 : 15,
-          }}
-        >
-          {name}
-        </Text>
+    // <View style={{ flexDirection: "row" }}>
+    <Container>
+      <ProfileImage source={{ uri }} size={size} />
+      <TextContainer>
+        <NameText isMe={isMe}>{name}</NameText>
         {!!introduction && (
           <View>
-            <View style={{ height: isMe ? 6 : 2 }} />
-            <Text style={{ fontSize: isMe ? 12 : 11, color: "grey" }}>
-              {introduction}
-            </Text>
+            <Margin height={isMe ? 6 : 2} />
+            <IntroductionText isMe={isMe}>{introduction}</IntroductionText>
           </View>
         )}
-      </View>
-    </View>
+      </TextContainer>
+    </Container>
+    // </View>
   );
 };
